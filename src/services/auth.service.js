be73bade8fef59d80fe1273618ec3aaf7761df60
satish <purohit.satish@amelio.co>
2022-09-06 18:05:49 +0530

    added api url

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import authHeader from "./auth-header";
 
-const API_URL = "/api/auth/";
+const API_URL = "https://test.api.com/api/auth/";
 
 class AuthService {
   login(username, password) {
